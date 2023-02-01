18350229aa9d56780c323c19f80b059405b4ff86
jangtjdgns <jangtjdgns0208@gmail.com>
2023-02-01 15:17:56 +0900

    무한루프 내에서 명령어 입력받기

--- src/com/KoreaIT/java/BasciAM/Main.java
+++ src/com/KoreaIT/java/BasciAM/Main.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-		
+
 		Scanner sc = new Scanner(System.in);
-	
-		System.out.printf("명령어) ");
-		String cmd = sc.nextLine();
-		
-		System.out.printf("입력된 명령어 : %s\n", cmd);
-		
+
+		while (true) {
+			System.out.printf("명령어) ");
+			String command = sc.nextLine();
+			
+			
+			//반복문 종료
+			if (command.equals("system exit")) {
+				break;
+			}
+		}
+
 		System.out.println("==프로그램 끝==");
-		
+
 		sc.close();
 	}
 }
